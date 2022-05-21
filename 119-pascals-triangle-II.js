b7a7cf39e84ead026ce48e640467bed289fda309
MusiurAlam <musiur.opu@gmail.com>
2022-05-22 00:30:38 +0600

    119-pascals-triangle-II solved

---
+++ 119-pascals-triangle-II.js
@@ -0,0 +1,29 @@
+var getRow = function (rowIndex) {
+  const numRows = rowIndex + 1;
+  let result = [];
+
+  let prevArr = [];
+
+  for (let i = 0; i < numRows; i++) {
+    let tempArr = [];
+
+    tempArr.length = prevArr.length + 1;
+
+    for (let j = 0; j < tempArr.length; j++) {
+      if (j === 0) {
+        tempArr[0] = 1;
+      } else if (j === tempArr.length - 1) {
+        tempArr[tempArr.length - 1] = 1;
+      } else {
+        tempArr[j] = prevArr[j] + prevArr[j - 1];
+      }
+    }
+
+    result.push(tempArr);
+    prevArr = tempArr;
+  }
+
+  return result[rowIndex];
+};
+
+console.log(getRow(1));
