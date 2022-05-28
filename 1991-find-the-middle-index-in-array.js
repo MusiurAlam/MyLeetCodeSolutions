1624bb9cb2ab63217a41b1c393b5806f5d4f54b6
MusiurAlam <musiur.opu@gmail.com>
2022-05-28 19:50:40 +0600

    1991-find-the-middle-index-in-array solved

---
+++ 1991-find-the-middle-index-in-array.js
@@ -0,0 +1,29 @@
+var findMiddleIndex = function (nums) {
+    let leftSum = 0,
+      rightSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (i === 0) {
+        leftSum = 0;
+        rightSum = nums.slice(1).reduce((acc, cur) => (acc += cur), 0);
+      } else if (i === nums.length - 1) {
+        rightSum = 0;
+        leftSum = nums
+          .slice(0, nums.length - 1)
+          .reduce((acc, cur) => (acc += cur), 0);
+      } else {
+        leftSum = nums.slice(0, i).reduce((acc, cur) => (acc += cur), 0);
+        rightSum = nums
+          .slice(i + 1, nums.length)
+          .reduce((acc, cur) => (acc += cur), 0);
+  
+      }
+  
+      if (leftSum === rightSum) {
+        return i;
+      }
+    }
+  
+    return -1;
+  };
+
+  console.log(findMiddleIndex([2,5]))
