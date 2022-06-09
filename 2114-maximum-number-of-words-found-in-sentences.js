57c7c326e5f3a45a3510955ed44d13ecd5cfaad1
MusiurAlam <musiur.opu@gmail.com>
2022-06-09 18:34:21 +0600

    2114-maximum-number-of-words-found-in-sentences solved

--- 2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences.js
@@ -1,7 +1,14 @@
 var mostWordsFound = function(sentences) {
-    let sentences = sentences.map(item => item.split(" "));
+    sentences = sentences.map(item => item.split(" ").length);
 
-    return sentences;
+    let max = 0;
+    for(let i = 0; i < sentences.length; i++){
+        if(sentences[i] > max){
+            max = sentences[i]
+        }
+    }
+
+    return max;
 };
 
-console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
+console.log(mostWordsFound(["please wait", "continue to fight", "continue to win"]))
